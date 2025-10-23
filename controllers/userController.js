761d7750cc66ef9f0d9667000ff1a438fa5df0bf
TheresiaSamantha <theresiasamanthamulyadi@gmail.com>
2025-10-23 07:56:22 +0700

    Menambahkan proses log in, namun belum ada pembatasan halaman

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { Student } = require('../models')
 const { Op } = require('sequelize')
+const bcrypt = require('bcryptjs') 
 
 class UserController {
     static async registerForm(req, res) {
@@ -24,12 +25,33 @@ class UserController {
     }
     static async loginForm(req, res) {
         try {
-            res.render("login")
+            const {error} = req.query;
+            res.render("login", {error})
         } catch (err) {
             res.send(err)
         }
     }
     static async postLogin(req, res) {
+        try {
+            const { name, password } = req.body
+            const user = await Student.findOne({where: {name: name}})
+            const error = 'invalid username/password'
+            if (user) {
+                const isValidPassword = bcrypt.compareSync(password, user.password);
+                if (isValidPassword) {
+                    return res.redirect('/')
+                } else {
+                    res.redirect(`/login?error=${error}`)
+                }
+            } else {
+                res.redirect(`/login?error=${error}`)
+            }
+            // res.send(user)
+        } catch (err) {
+            res.send(err)
+        }
+    }
+    static async getLogout(req, res) {
         try {
             res.send("test")
         } catch (err) {
